d566414f84b84ecfa79a10ab97958aa44845d05d
ChristofferRysholt <eaacrch@students.eaaa.dk>
2019-02-24 16:51:08 +0100

    Add files via upload

---
+++ website/js/projekter.js
@@ -0,0 +1,9 @@
+function changeImageBibliotek(imgNameBibliotek) {
+   image = document.getElementById('nubio');
+   image.src = imgNameBibliotek;
+}
+
+function changeImageWebdock(imgNameWebdock) {
+   image = document.getElementById('webdock');
+   image.src = imgNameWebdock;
+}
